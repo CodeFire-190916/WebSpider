14998730a6264437b55a43118ed7d634572dbfb1
Alex Melnyk <melnyk@codefire.com.ua>
2016-12-07 20:59:43 +0200

    Regex added. StringBuilder added for storing HTML.

--- src/java/ua/com/codefire/net/Spider.java
+++ src/java/ua/com/codefire/net/Spider.java
@@ -7,6 +7,7 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -14,12 +15,14 @@ import java.util.regex.Pattern;
  */
 public class Spider {
 
-    private static final String ADDRESS_REGEX = "<URL_REGEX>";
+
+    //<a href="ua/navchannya-u-maup/navchalno-vihovnij-kompleks-prestizh.html" >
+    private static final String ADDRESS_REGEX = "href=\"(?<url>[^\"]+)";
     private static final Pattern ADDRESS_PATTERN;
 
     // Static Initializer
     static {
-        ADDRESS_PATTERN = Pattern.compile(ADDRESS_REGEX);
+        ADDRESS_PATTERN = Pattern.compile(ADDRESS_REGEX, Pattern.MULTILINE);
     }
 
     public List<String> scan(String address) {
@@ -29,15 +32,24 @@ public class Spider {
             URL url = new URL(address);
             URLConnection conn = url.openConnection();
 
+            StringBuilder buffer = new StringBuilder();
+
             try (Scanner scanner = new Scanner(conn.getInputStream())) {
-
-                while (scanner.hasNext(ADDRESS_PATTERN)) {
-                    String foundAddress = scanner.next(ADDRESS_PATTERN);
-
-                    foundAddressList.add(foundAddress);
+                while (scanner.hasNextLine()) {
+                    buffer.append(scanner.nextLine()).append("\n");
                 }
-
             }
+
+            Matcher matcher = ADDRESS_PATTERN.matcher(buffer);
+
+            while (matcher.find()) {
+//                String foundAddress = matcher.group(1);
+                String foundAddress = matcher.group("url");
+
+                foundAddressList.add(foundAddress);
+            }
+
+            System.out.println(foundAddressList);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
